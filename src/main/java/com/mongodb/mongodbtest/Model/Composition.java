11ac3dd456aa3a39e84f653e8ad90342a8d1edf1
SalvaMiraMori <salvamiramori@gmail.com>
2024-11-29 13:48:12 +0100

    Clean up

--- src/main/java/com/mongodb/mongodbtest/Model/Composition.java
+++ src/main/java/com/mongodb/mongodbtest/Model/Composition.java
@@ -11,20 +11,10 @@ import java.util.Map;
 @Document(collection = "compositions")
 @Data
 public class Composition {
-    // @Id
-    // private String _id;
-    // private String compositionType;
-    // private String ehrId;
-    // private String timestamp;
     private RelatedTo relatedTo;
     private Map<String, Object> aqlSearchParams;
-    // private List<Relation> RelatedTo;
-    // private List<EnrichedData> EnrichedData;
     private String canonicalJSON;
 
-    // public record AQLSearchParams(Map<String, Object> AQLSearchParams) {}
-    // public record Relation(String entity, String id) {}
-    // public record EnrichedData(List<String> tags, List<Double> similarityVector) {}
     public record RelatedTo(String ehrId, String compositionId, String templateId, List<String> archetypes) {}
 
     public String getCanonicalJSON(){ return canonicalJSON; }
